da8121e35c086ba7ff70952bcb74e0bf6070f33a
Arseniy Kouzmenkov <arseniy.kouzmenkov@gmail.com>
2020-03-28 20:17:55 -0600

    backup before starting to work on decision trees

--- big_five_classification_approaches.py
+++ big_five_classification_approaches.py
@@ -16,7 +16,7 @@ def KNNClassifierWithTuning( X_train, y_train, X_val, y_val, n_neighbors=None, l
     acc_train = [[],[],[]]
     best_acc = 0
     model = None
-    model_acc
+    model_acc = 0
     best_n_neighbors = None
     best_leaf_size = None
     best_p = 0
@@ -25,13 +25,13 @@ def KNNClassifierWithTuning( X_train, y_train, X_val, y_val, n_neighbors=None, l
     if n_neighbors is not None:
         neighs = n_neighbors
     else:
-        neighs = [1] + list( range( 10 : 50 : 10 ) )
+        neighs = [1] + list( range( 10, 60, 10 ) )
     
     leafs = None
     if leaf_size is not None:
         leafs = leaf_size
     else:
-        leafs = [1] + list( range( 5 : 30 : 5 ) )
+        leafs = [1] + list( range( 5, 35, 5 ) )
 
     ps = None
     if p is not None:
@@ -40,7 +40,7 @@ def KNNClassifierWithTuning( X_train, y_train, X_val, y_val, n_neighbors=None, l
         ps = [1,2]
 
     for neigh in neighs:
-        knn = neighbors.KNeighborsClassifier( n_neighbors=neigh )
+        knn = neighbors.KNeighborsClassifier( n_neighbors=neigh, algorithm='kd_tree', weights='distance' )
         print( "Tuning n_neighbors. Current n being tested:", neigh, ". Time stamp:", str( datetime.datetime.now() ) )
         knn.fit( X_train, y_train )
         print( "Finished fitting. Starting scoring on validation set. Time stamp:", str( datetime.datetime.now() ) )
@@ -51,12 +51,14 @@ def KNNClassifierWithTuning( X_train, y_train, X_val, y_val, n_neighbors=None, l
             best_acc = acc
             print( "Current best accuracy for neighbor tuning achieved!" )
             best_n_neighbors = neigh
+        print()
     
     print( "Finished tuning n_neighbors. Starting to tune leaf size." )
+    print( "-"*40 )
     best_acc = 0
     
     for leaf in leafs:
-        knn = neighbors.KNeighborsClassifier( leaf_size=leaf )
+        knn = neighbors.KNeighborsClassifier( leaf_size=leaf, algorithm='kd_tree', weights='distance' )
         print( "Tuning leaf size. Current leaf size being tested:", leaf, ". Time stamp:", str( datetime.datetime.now() ) )
         knn.fit( X_train, y_train )
         print( "Finished fitting. Starting scoring on validation set. Time stamp:", str( datetime.datetime.now() ) )
@@ -67,12 +69,14 @@ def KNNClassifierWithTuning( X_train, y_train, X_val, y_val, n_neighbors=None, l
             best_acc = acc
             print( "Current best accuracy for leaf tuning achieved!" )
             best_leaf_size = leaf
+        print()
 
     print( "Finished tuning leaf size. Starting to tune regularization" )
+    print( "-"*40 )
     best_acc = 0
 
     for p in ps:
-        knn = neighbors.KNeighborsClassifier( p=p )
+        knn = neighbors.KNeighborsClassifier( p=p, algorithm='kd_tree', weights='distance' )
         print( "Tuning regularization. Current p being tested:", p, ". Time stamp:", str( datetime.datetime.now() ) )
         knn.fit( X_train, y_train )
         print( "Finished fitting. Starting scoring on validation set. Time stamp:", str( datetime.datetime.now() ) )
@@ -83,11 +87,13 @@ def KNNClassifierWithTuning( X_train, y_train, X_val, y_val, n_neighbors=None, l
             best_acc = acc
             print( "Current best accuracy for leaf tuning achieved!" )
             best_p = p
+        print()
 
     print( "Finished tuning regularization. Building the model with the best hyperparameters" )
+    print( "-"*40 )
     
-    model = neighbots.KNeighborsClassifier( n_neighbots=best_n_neighbors,
-            leaf_size=best_leaf_size, p=best_p )
+    model = neighbors.KNeighborsClassifier( n_neighbors=best_n_neighbors,
+            leaf_size=best_leaf_size, p=best_p, algorithm='kd_tree', weights='distance' )
     print( "Starting to fit the model with hyperparameters n_neighbors =", best_n_neighbors, "leaf size =", best_leaf_size, "and p =", best_p, ". Time stamp:", str( datetime.datetime.now() ) )
     
     model.fit( X_train, y_train )
@@ -165,13 +171,32 @@ def main():
     X_train, y_train, X_val, y_val, X_test, y_test =  \
             setUpData( str( sys.argv[1] ) ) 
     print( X_train, y_train, X_val, y_val, X_test, y_test )
+    """
     #KNNModel, KNN_acc = KNNClassifier( X_train, y_train, X_val, y_val, 15, 5, 1 )
-    KNNModel, KNNModel_val_acc, KNN_acc_train, KNN_best_n_neighbors, KNN_best_leaf_size, KNN_best_p = KNNClassifierWithTuning( X_train, y_train, X_val, y_val )
+    #n = [30]
+    #l = [10]
+    #p = [1]
+    #n = [1] + list( range( 2, 52, 2 ) )
+    n = list( range( 30, 38 ) )
+    #l = [1] + list( range( 2, 36 ) )
+    #l = list( range( 3, 10 ) )
+    #p = [1]
+    n = [34]
+    l = [7]
+    p = [1]
+    KNNModel, KNNModel_val_acc, KNN_acc_train, KNN_best_n_neighbors, KNN_best_leaf_size, KNN_best_p = KNNClassifierWithTuning( X_train, y_train, X_val, y_val, n, l, p )
     #print( "Validation accuracy:", KNN_acc*100 )
     #print( "Test accuracy:", KNNModel.score( X_test, y_test )*100 )
-    print( "KNN Model validation accuracy:", str(KNNModel_acc*100) + "%", "Scoring test accuracy." )
+    #KNNModel = neighbors.KNeighborsClassifier( n_neighbors=n[0], leaf_size=l[0], p=p[0] )
+    #KNNModel.fit( X_train, y_train )
+    #KNNModel_val_acc = KNNModel.score( X_val, y_val )
+    print( "KNN Model validation accuracy:", str(KNNModel_val_acc*100) + "%", "Scoring test accuracy." )
     KNNModel_test_acc = KNNModel.score( X_test, y_test )
     print( "KNN Model test accuract:", str( KNNModel_test_acc*100) + "%" ) 
+    #np.set_printoptions( threshold=sys.maxsize )
+    print( KNNModel.predict( X_test ) )
+    """
+
 
     pass
 
